53f505b6caf2d796d57e4573a202c663b2f8947f
ljn <ljnchn@qq.com>
2022-11-08 15:19:46 +0800

    处理跨域

---
+++ app/Middleware/AccessControl.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Middleware;
+
+use Webman\Http\Request;
+use Webman\Http\Response;
+use Webman\MiddlewareInterface;
+
+class AccessControl implements MiddlewareInterface
+{
+    public function process(Request $request, callable $handler) : Response
+    {
+        $headers = [
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Origin' => $request->header('Origin', '*'),
+            'Access-Control-Allow-Methods' => '*',
+            'Access-Control-Allow-Headers' => '*',
+        ];
+        $response = $request->method() == 'OPTIONS' ? response() : $handler($request);
+
+        // 给响应添加跨域相关的http头
+        $response->withHeaders($headers);
+
+        return $response;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,6 @@ Route::get('/api', [\App\Api\Controller\Index::class, 'index']);
 Route::group('/api', function () {
     Route::get('/index', [\App\Api\Controller\Index::class, 'index']);
     Route::get('/user/{uid}', [\App\Api\Controller\Index::class, 'user']);
-})->middleware([]);
+})->middleware([
+    App\Middleware\AccessControl::class,
+]);
